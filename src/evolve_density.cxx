43a43b17212809e4d5f43fe20a9aa4e90e0beb12
totork <ttork96@gmail.com>
2025-06-18 16:10:00 +0200

    Added finite density floor

--- src/evolve_density.cxx
+++ src/evolve_density.cxx
@@ -188,7 +188,7 @@ void EvolveDensity::transform(Options& state) {
   }
 
   auto& species = state["species"][name];
-  set(species["density"], floor(N, 0.0)); // Density in state always >= 0
+  set(species["density"], floor(N, density_floor)); // Density in state always >= 0
   set(species["AA"], AA);                 // Atomic mass
   if (charge != 0.0) {                    // Don't set charge for neutral species
     set(species["charge"], charge);
