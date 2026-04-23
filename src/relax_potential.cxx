5b63b4332b68b26902b94af5773e392ca9019812
totork <ttork96@gmail.com>
2026-04-23 16:00:33 +0200

    Allow for non-boussinesq in relax potential

--- src/relax_potential.cxx
+++ src/relax_potential.cxx
@@ -418,8 +418,8 @@ void RelaxPotential::finally(const Options& state) {
     }
   } else {
     // Non-Boussinesq. Calculate mass density by summing over species
-    throw BoutException("Non_boussinesq not implemented");
     // Calculate vorticity from potential phi
+    Field3D flow_xlow_phi,flow_zlow_phi;
     Field3D phi_vort = 0.0;
     for (auto& kv : allspecies.getChildren()) {
       const Options& species = kv.second;
@@ -434,12 +434,12 @@ void RelaxPotential::finally(const Options& state) {
 
       const BoutReal Ai = get<BoutReal>(species["AA"]);
       const Field3D Ni = get<Field3D>(species["density"]);
-      phi_vort += Div_a_Grad_perp((Ai / Bsq) * Ni, phi);
+      phi_vort += (*dagp)((Ai / Bsq) * Ni, phi, flow_xlow_phi, flow_zlow_phi, false);
 
       if (diamagnetic_polarisation and species.isSet("pressure")) {
         // Calculate the diamagnetic flow contribution
         const Field3D Pi = get<Field3D>(species["pressure"]);
-        phi_vort += Div_a_Grad_perp(Ai / Bsq, Pi);
+        phi_vort += (*dagp)(Ai / Bsq, Pi, flow_xlow_phi, flow_zlow_phi, false);
       }
     }
 
