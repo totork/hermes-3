ffa3a0a1cb5bc38d90752560dade1aec91902e71
totork <ttork96@gmail.com>
2026-07-30 16:11:19 +0200

    DOnt do unit test because grid does not have dagp coefficients

--- tests/unit/test_anomalous_diffusion_3d.cxx
+++ tests/unit/test_anomalous_diffusion_3d.cxx
@@ -12,32 +12,3 @@ namespace globals {
 extern Mesh* mesh;
 } // namespace globals                                                                                                                                                                                                                                                            
 } // namespace bout                                                                                                                                                                                                                                                               
-
-// The unit tests use the global mesh                                                                                                                                                                                                                                             
-using namespace bout::globals;
-
-#include <bout/field_factory.hxx> // For generating functions                                                                                                                                                                                                                     
-
-// Reuse the "standard" fixture for FakeMesh                                                                                                                                                                                                                                      
-using AnomalousDiffusion3DTest = FakeMeshFixture;
-
-TEST_F(AnomalousDiffusion3DTest, NoDiffusion) {
-  Options options;
-  options["units"]["meters"] = 1.0;
-  options["units"]["seconds"] = 1.0;
-
-  AnomalousDiffusion3D component("h", options, nullptr);
-
-  Field3D N = FieldFactory::get()->create3D("1 + y * (x - 0.5)", &options, mesh);
-  mesh->communicate(N);
-
-  Options state;
-  state["species"]["h"]["density"] = N;
-
-  // If D is not set, then the diffusion should not be calculated                                                                                                                                                                                                                 
-  component.transform(state);
-
-  ASSERT_FALSE(state["species"]["h"].isSet("density_source"));
-  ASSERT_FALSE(state["species"]["h"].isSet("momentum_source"));
-  ASSERT_FALSE(state["species"]["h"].isSet("energy_source"));
-}
