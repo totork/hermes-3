a65869a3ee7ece88b436ea5483bfeb978a4c4619
totork <ttork96@gmail.com>
2026-07-31 11:56:50 +0200

    Adjust quasineutral component for fci

--- src/quasineutral.cxx
+++ src/quasineutral.cxx
@@ -5,6 +5,7 @@
 
 #include <numeric> // for accumulate
 #include <string>
+using bout::globals::mesh;
 
 Quasineutral::Quasineutral(std::string name, Options& alloptions, Solver* UNUSED(solver))
     : NamedComponent(name,
@@ -30,30 +31,69 @@ void Quasineutral::transform_impl(GuardedOptions& state) {
   std::map<std::string, GuardedOptions> children = allspecies.getChildren();
 
   // Add charge density of other species
-  const Field3D rho = std::accumulate(
-      // Iterate through species
-      begin(children), end(children),
-      // Start with no charge
-      Field3D(0.0),
-      [this](Field3D value,
-             const std::map<std::string, GuardedOptions>::value_type& name_species) {
-        const GuardedOptions species = name_species.second;
-        // Add other species which have density and charge
-        if (name_species.first != name and species.isSet("charge")
-            and species.isSet("density")) {
-          // Note: Not assuming that the boundary has been set
-          return Field3D{value
-                         + getNoBoundary<Field3D>(species["density"])
+  Field3D rho;
+
+  if (mesh->isFci()) {
+    rho = 0.0;
+    rho.applyBoundary("neumann");
+    mesh->communicate(rho);
+    rho.applyParallelBoundary("parallel_neumann_o1");
+    for (auto& kv : children) {
+      GuardedOptions species = allspecies[kv.first]; // Note: Need non-const
+      
+      if (kv.first == name) {
+	continue;
+      }
+
+      if (!species.isSet("charge")) {
+	continue;
+      }
+      
+      auto q = get<BoutReal>(species["charge"]);
+      if (fabs(q) < 1e-5 ) {
+	continue;
+      }
+
+      if (!species.isSet("density")) {
+	continue;
+      }
+      Field3D new_rho = getNoBoundary<Field3D>(species["density"]).asField3DParallel() * q;
+      rho = rho.asField3DParallel() + new_rho.asField3DParallel();
+      
+    }
+    ASSERT2(rho.hasParallelSlices());
+    
+  } else {
+    rho= std::accumulate(
+			 // Iterate through species
+			 begin(children), end(children),
+			 // Start with no charge
+			 Field3D(0.0),
+			 [this](Field3D value,
+				const std::map<std::string, GuardedOptions>::value_type& name_species) {
+			   const GuardedOptions species = name_species.second;
+			   // Add other species which have density and charge
+			   if (name_species.first != name and species.isSet("charge")
+			       and species.isSet("density")) {
+			     // Note: Not assuming that the boundary has been set
+			     return Field3D{value
+			       + getNoBoundary<Field3D>(species["density"])
                                * get<BoutReal>(species["charge"])};
-        }
-        return value;
-      });
+			   }
+			   return value;
+			 });
+  }
 
   // Set quantites for this species
   GuardedOptions species = allspecies[name];
 
   // Calculate density required. Floor so that density is >= 0
-  density = floor(Field3D{rho / (-charge)}, 0.0);
+  if (mesh->isFci()) {
+    density = floor(rho.asField3DParallel() / (-charge), 0.0);
+  } else {
+    density = floor(Field3D{rho / (-charge)}, 0.0);
+  }
+  
   set(species["density"], density);
 
   set(species["charge"], charge);
